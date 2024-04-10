a837b10995eafb088a9bcac6b2faa77b9236c7d1
vivektemptest <vivektemptest@gmail.com>
2024-04-11 00:53:47 +0530

    new changes on my locak

---
+++ resources/js/Components/CreatePostOverlay.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref, reactive } from "vue";
+import { router, usePage } from "@inertiajs/vue3";
+
+import Close from "vue-material-design-icons/Close.vue";
+import ArrowLeft from "vue-material-design-icons/ArrowLeft.vue";
+import MapMarkerOutline from "vue-material-design-icons/MapMarkerOutline.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+
+const emit = defineEmits(["close"]);
+
+const form = reactive({
+    text: null,
+    file: null,
+});
+
+let isValidFile = ref(null);
+let fileDisplay = ref("");
+let textarea = ref("");
+let error = ref({
+    text: null,
+    file: null,
+});
+
+const closeOverlay = () => {
+    form.text = null;
+    form.file = null;
+    fileDisplay.value = "";
+    emit("close");
+};
+</script>
+
+<template>
+    <div
+        id="OverlaySection"
+        class="fixed z-50 top-0 left-0 w-full h-screen bg-[#000000] bg-opacity-60 p-3"
+    >
+        <Button
+            type="button"
+            class="absolute right-3 cursor-pointer"
+            @click="closeOverlay()"
+        >
+            <Close :size="27" fillColor="#ffffff" />
+        </Button>
+
+        <div
+            class="max-w-6xl h-[calc(100%-100px)] mx-auto mt-10 bg-white rounded-xl"
+        >
+        <div class="flex items-center justify-between w-full rounded-t-xl p-3 border-b-gray-300">
+            <ArrowLeft :size="30" fillColor="#000000" @click="$event=>closeOverlay()"/>
+                <div class="text-lg font-extrabold">New Reel</div>
+                <button class="text-lg text-blue-500 hover:text-gray-900 font-extrabold">Share</button>
+        </div>
+    </div>
+    </div>
+</template>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -12,7 +12,9 @@ import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
 import AccountPlusOutline from "vue-material-design-icons/AccountPlusOutline.vue";
 import Menu from "vue-material-design-icons/Menu.vue";
 
-import MenuItem from '@/Components/MenuItem.vue'
+import MenuItem from "@/Components/MenuItem.vue";
+import CreatePostOverlay from '@/Components/CreatePostOverlay.vue'
+
 
 let showCreatePost = ref(false);
 </script>
@@ -37,48 +39,202 @@ let showCreatePost = ref(false);
                         class="flex items-center w-full bg-gray-100 rounded-lg"
                     >
                         <Magnify class="pl-4" fillColor="#BEBEBE" :size="27" />
-                        <input type="text" class="bg-transparent w-full placeholder-[#BEBEBE] border-0 ring-0 focus:ring-0" placeholder="Search">
+                        <input
+                            type="text"
+                            class="bg-transparent w-full placeholder-[#BEBEBE] border-0 ring-0 focus:ring-0"
+                            placeholder="Search"
+                        />
                     </div>
-                    <HeartOutline class="pl-4 pr-3" fillColor="#000000" :size="27"/>
+                    <HeartOutline
+                        class="pl-4 pr-3"
+                        fillColor="#000000"
+                        :size="27"
+                    />
                 </div>
             </div>
         </div>
-        <div v-if="$page.url!=='/'" id="TopNavUser" class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300">
+        <div
+            v-if="$page.url !== '/'"
+            id="TopNavUser"
+            class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
             <Link href="/" class="px-4">
-                <ChevronLeft :size="30" class="cursor-pointer"/>
+                <ChevronLeft :size="30" class="cursor-pointer" />
             </Link>
             <div class="font-extrabold text-lg">Name Here</div>
-            <AccountPlusOutline :size="30" class="cursor-pointer px-4"/>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
         </div>
 
-        <div id="SideNav" class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300">
+        <div
+            id="SideNav"
+            class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300"
+        >
             <Link href="/">
-                <img src="/insta-logo-small.png" alt="" class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer">
-                <img src="/insta-logo.png" alt="" class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer">
+                <img
+                    src="/insta-logo-small.png"
+                    alt=""
+                    class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer"
+                />
+                <img
+                    src="/insta-logo.png"
+                    alt=""
+                    class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer"
+                />
             </Link>
             <div class="px-3">
                 <Link href="/">
-                    <MenuItem iconString="Home" class="mb-4"/>
+                    <MenuItem iconString="Home" class="mb-4" />
                 </Link>
-                <MenuItem iconString="Search" class="mb-4"/>
-                <MenuItem iconString="Explore" class="mb-4"/>
-                <MenuItem iconString="Messages" class="mb-4"/>
-                <MenuItem iconString="Notification" class="mb-4"/>
-                <MenuItem iconString="Create" class="mb-4"/>
-                <MenuItem iconString="Profile" class="mb-4"/>
+                <MenuItem iconString="Search" class="mb-4" />
+                <MenuItem iconString="Explore" class="mb-4" />
+                <MenuItem iconString="Messages" class="mb-4" />
+                <MenuItem iconString="Notification" class="mb-4" />
+                <MenuItem
+                    iconString="Create"
+                    class="mb-4"
+                    @click="($event) => (showCreatePost = true)"
+                />
+                <Link href="/">
+                    <MenuItem iconString="Profile" class="mb-4" />
+                </Link>
             </div>
             <Link href="/" class="absolute bottom-0 px-3 w-full">
-                <MenuItem iconString="Log out" class="mb-4"/>
+                <MenuItem iconString="Log out" class="mb-4" />
             </Link>
         </div>
 
-        <div class="flex lg:justify-between bg-white h-full w-[100%-280px] xl:pl-[280px] lg:pl-[100px] overflow-auto">
-            <div class="mx-auto md:pt-6 pt-20"
-            :class="$page.url==='/'?'lg:w-8/12 w-full':'max-w-[1200px]'">
+        <div
+            class="flex lg:justify-between bg-white h-full w-[100%-280px] xl:pl-[280px] lg:pl-[100px] overflow-auto"
+        >
+            <div
+                class="mx-auto md:pt-6 pt-20"
+                :class="
+                    $page.url === '/' ? 'lg:w-8/12 w-full' : 'max-w-[1200px]'
+                "
+            >
                 <main>
-                    <slot/>
+                    <slot />
                 </main>
             </div>
+            <div
+                v-if="$page.url === '/'"
+                id="SuggestionSection"
+                class="lg:w-4/12 lg:block hidden text-black mt-100"
+            >
+                <Link
+                    href="/"
+                    class="flex items-center justify-between max-w-[300px]"
+                >
+                    <div class="flex items-center">
+                        <img
+                            class="rounded-full z-10 w-[58px] h-[58px]"
+                            src="https://picsum.photos/id/50/300/320"
+                            alt=""
+                            srcset=""
+                        />
+                        <div class="pl-4">
+                            <div class="text-black font-extrabold">
+                                Name here
+                            </div>
+                            <div class="text-gray-500 text-extrabold text-sm">
+                                Name here
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        class="text-blue-500 hover:text-gray-900 text-xs font-extrabold"
+                    >
+                        Switch
+                    </button>
+                </Link>
+
+                <div
+                    class="max-w-[300px] flex items-center justify-between py-3"
+                >
+                    <div class="text-gray-500 font-extrabold">
+                        Suggestions for you
+                    </div>
+                    <button
+                        class="text-blue-500 hover:text-gray-900 text-xs font-extrabold"
+                    >
+                        See All
+                    </button>
+                </div>
+                <Link
+                    href="/"
+                    class="flex items-center justify-between max-w-[300px] pb-2"
+                >
+                    <div class="flex items-center">
+                        <img
+                            class="rounded-full z-10 w-[58px] h-[58px]"
+                            src="https://picsum.photos/id/50/300/320"
+                            alt=""
+                            srcset=""
+                        />
+                        <div class="pl-4">
+                            <div class="text-black font-extrabold">
+                                Name here
+                            </div>
+                            <div class="text-gray-500 text-extrabold text-sm">
+                                Name here
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        class="text-blue-500 hover:text-gray-900 text-xs font-extrabold"
+                    >
+                        Follow
+                    </button>
+                </Link>
+                <div class="max-w-[300px] mt-5">
+                    <div class="tetxt-sm text-gray-400">
+                        About help press api privacy terms location
+                    </div>
+                    <div class="text-left text-gray-400 mt-4">
+                        C 2023 INSTAGRAM FROM META
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div
+            id="BottomNav"
+            class="fixed x-30 bottom-0 w-full md:hidden flex items-center justify-around bg-white border-t pyt-2 border-t-gray-300"
+        >
+            <Link href="/">
+                <HomeOutline
+                    fillColor="#000000"
+                    :size="33"
+                    class="cursor-pointer"
+                />
+            </Link>
+            <Compass fillColor="#000000" :size="33" class="cursor-pointer" />
+            <SendOutline
+                fillColor="#000000"
+                :size="33"
+                class="cursor-pointer"
+            />
+            <Plus
+                fillColor="#000000"
+                :size="33"
+                class="cursor-pointer"
+                @click="($event) => (showCreatePost = true)"
+            />
+            <AccountOutline
+                fillColor="#000000"
+                :size="33"
+                class="cursor-pointer"
+            />
+            <Link href="/">
+                <img
+                    class="rounded-full \ w-[30px] cursor-pointer"
+                    src="https://picsum.photos/id/50/300/320"
+                    alt=""
+                    srcset=""
+                />
+            </Link>
         </div>
     </div>
+
+    <CreatePostOverlay v-if="showCreatePost" @close="showCreatePost = false" />
 </template>
